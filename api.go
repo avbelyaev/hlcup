19c285f5da964197681f7e377ff48fe3e1284414
anthony <antonsdl@gmail.com>
2019-01-06 14:09:57 +0300

    added like model validation

--- api.go
+++ api.go
@@ -4,16 +4,23 @@ import (
 	"net/http"
 	"hlcup/domain"
 	"encoding/json"
+	log "github.com/Sirupsen/logrus"
 )
 
 func (s *Server) createAccount(writer http.ResponseWriter, request *http.Request) {
+	// check method
+	if http.MethodPost != request.Method {
+		badRequest(writer, nil)
+		return
+	}
+
 	var newAccount domain.Account
 	var decoder = json.NewDecoder(request.Body)
 
 	// decode
 	var err = decoder.Decode(&newAccount)
 	if nil != err {
-		s.badRequest(writer, err)
+		badRequest(writer, err)
 		return
 	}
 
@@ -21,35 +28,42 @@ func (s *Server) createAccount(writer http.ResponseWriter, request *http.Request
 	s.store[newAccount.ID] = newAccount
 
 	// rsp
-	writer.WriteHeader(200)
+	writer.WriteHeader(201)
 	writer.Write(nil)
 }
 
 func (s *Server) addLikes(writer http.ResponseWriter, request *http.Request) {
-	var newLikes domain.Like // TODO likeS
+	// check method
+	if http.MethodPost != request.Method {
+		badRequest(writer, nil)
+		return
+	}
+
+	var newLikes domain.Likes
 	var decoder = json.NewDecoder(request.Body)
 
 	// decode
 	var err = decoder.Decode(&newLikes)
 	if nil != err {
-		s.badRequest(writer, err)
+		badRequest(writer, err)
 		return
 	}
 
 	// validate
 	err = newLikes.Validate()
 	if nil != err {
-		s.badRequest(writer, err)
+		badRequest(writer, err)
 		return
 	}
+	// TODO validate that liker and likee exist
 
 	// rsp
-	writer.WriteHeader(200)
-	writer.Write([]byte("good"))
+	writer.WriteHeader(202)
+	writer.Write(nil)
 }
 
-func (s *Server) badRequest(writer http.ResponseWriter, err error) {
-	s.log.WithError(err).Error("bad rq")
+func badRequest(writer http.ResponseWriter, err error) {
+	log.WithError(err).Error("bad rq")
 	http.Error(writer, "go fck yourself", 400)
 	writer.Write(nil)
 }
--- domain/like.go
+++ domain/like.go
@@ -1,19 +1,51 @@
 package domain
 
-import "github.com/go-ozzo/ozzo-validation"
+import (
+	"github.com/go-ozzo/ozzo-validation"
+	"time"
+	"github.com/pkg/errors"
+)
 
 type Likes struct {
 	Likes []Like `json:"likes"`
 }
 
 type Like struct {
-	Likee int `json:"likee"`
-	Ts    int `json:"ts"`
-	Liker int `json:"liker"`
+	Likee int   `json:"likee,omitempty"`
+	Ts    int64 `json:"ts,omitempty"`
+	Liker int   `json:"liker,omitempty"`
 }
 
-// TODO not needed
-func (lk *Like) Validate() error {
-	return validation.ValidateStruct(lk,
-		validation.Field(&lk.Likee, validation.Required), )
+func (likes *Likes) Validate() error {
+	var now = time.Now().UTC().Unix()
+
+	for _, like := range likes.Likes {
+		// fail on first error
+		var err = like.validate(now)
+		if nil != err {
+			return errors.New("invalid like: " + err.Error())
+		}
+	}
+	return nil
 }
+
+func (like *Like) validate(tsMax int64) error {
+	return validation.ValidateStruct(like,
+		validation.Field(&like.Ts, validation.Required, validation.Max(tsMax)),
+		validation.Field(&like.Likee, validation.Required),
+		validation.Field(&like.Liker, validation.Required),
+	)
+}
+
+//func validateTimestamp(ranger ...int64) func(value interface{}) error {
+//	return func(value interface{}) error {
+//		var v = value.(int64)
+//		if 0 < len(ranger) && v < ranger[0] {
+//			return errors.New("too small")
+//		}
+//		if 1 < len(ranger) && v > ranger[1] {
+//			return errors.New("")
+//		}
+//		return nil
+//	}
+//}
