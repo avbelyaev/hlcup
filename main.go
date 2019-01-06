437ed9b3f9df6dd3f30b6fcd7f4a75560db3f254
anthony <antonsdl@gmail.com>
2019-01-06 16:37:18 +0300

    added gorilla router to validate http methods automatically

--- main.go
+++ main.go
@@ -4,47 +4,37 @@ import (
 	"net/http"
 	logger "github.com/Sirupsen/logrus"
 	"hlcup/domain"
+	"github.com/gorilla/mux"
 )
 
-//
-//type router struct {
-//
-//}
-//
 type Server struct {
 	log   *logger.Logger
 	store map[int]domain.Account
 }
-//
-//func NewRouter() *router {
-//	return &router{
-//		// empty
-//	}
-//}
-//
+
 func NewServer() *Server {
 	return &Server{
 		log:   logger.New(),
 		store: make(map[int]domain.Account),
 	}
 }
-//
-//func (s *Server) serve() {
-//
-//}
 
+func (s *Server) setupRouter() {
+	var r = mux.NewRouter()
+	r.HandleFunc("/accounts/new", s.createAccount).Methods("POST")
+	r.HandleFunc("/accounts/likes", s.addLikes).Methods("POST")
+	http.Handle("/", r)
+}
 
 func main() {
 	var s = NewServer()
+	s.setupRouter()
 
 	var err = s.loadInitialData()
 	if nil != err {
-		s.log.Fatal("Could not load initial data")
+		s.log.Fatal("Could not load initial data. Terminating")
 		panic(err)
 	}
-
-	http.HandleFunc("/accounts/new", s.createAccount)
-	http.HandleFunc("/accounts/likes", s.addLikes)
 
 	s.log.Info("starting dating service")
 	s.log.Fatal(http.ListenAndServe(":8080", nil))
